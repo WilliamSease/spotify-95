e3006490d10bfc3dc84082a9164479b3dc347257
WilliamASease <seasewa@appstate.edu>
2024-01-14 02:14:03 -0500

    releasepage

--- releasepage/src/App.tsx
+++ releasepage/src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import {
+  Anchor,
+  GroupBox,
+  styleReset,
+  Window,
+  WindowContent,
+  WindowHeader,
+} from 'react95';
+// pick a theme of your choice
+import original from 'react95/dist/themes/original';
+// original Windows95 font (optionally)
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
+const App = () => (
+  <div style={{ height: '100%' }}>
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+      <Window style={{ width: '100%', height: '100%' }}>
+        <WindowHeader>Spotify95 for React95</WindowHeader>
+        <WindowContent>
+          <GroupBox label="Downloads">
+            <Anchor
+              href="WilliamASease.github.io/spotify-95/releasepage/downloads/win-0.0.1.rar"
+              target="_blank"
+            >
+              Windows 0.0.1
+            </Anchor>
+          </GroupBox>
+        </WindowContent>
+      </Window>
+    </ThemeProvider>
+  </div>
+);
 
 export default App;
